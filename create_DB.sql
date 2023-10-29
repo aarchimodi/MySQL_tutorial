f82e5189174b4fae800c6dfd7130ebd186fa04b3
aarchi <117932380+aarchimodi@users.noreply.github.com>
2023-10-29 20:05:37 +0530

    create DB and use it

---
+++ create_DB.sql
@@ -0,0 +1,7 @@
+create database temp1;   -- to create a new database
+drop database temp1;   -- to delete created database
+
+create database temp2;
+drop database temp2;
+
+create database college;
