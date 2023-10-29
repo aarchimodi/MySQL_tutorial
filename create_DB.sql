2e5246d9be8cdba1df8a951669be043b94030dda
aarchi <117932380+aarchimodi@users.noreply.github.com>
2023-10-29 21:08:51 +0530

    "create" a DB and "use" it

--- create_DB.sql
+++ create_DB.sql
@@ -1,7 +1,10 @@
 create database temp1;   -- to create a new database
 drop database temp1;   -- to delete created database
 
-create database temp2;
-drop database temp2;
-
 create database college;
+use college;
+
+-- The USE statement of MySQL helps you to select/use a database. 
+-- You can also change to another database with this statement.
+-- Once you set the current database it will be same until the end of the session unless you change it.
+
