0d558fc99ca5d29aac958875f774125d7b57353a
aarchi <117932380+aarchimodi@users.noreply.github.com>
2023-11-04 14:44:25 +0530

    limit, ORDER_BY (asc, desc) clause

--- order_by.sql
+++ order_by.sql
@@ -8,5 +8,5 @@ select * from student where name = "rhythm" limit 2;
 -- in the ascending order names will be returned (a,b,c,d,e,f...)
 select * from student order by name asc;
 
--- 107, 106,105 in this order  
+-- 107, 106, 105 in this order id will be returned 
 select * from student order by id desc;
