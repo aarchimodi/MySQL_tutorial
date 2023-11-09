8d9c85e7a40e3c03925e800ac3e33c0f6647d0c4
aarchi <117932380+aarchimodi@users.noreply.github.com>
2023-11-09 13:35:34 +0530

    update is used to update existing row

---
+++ update.sql
@@ -0,0 +1,20 @@
+-- update is used to update the existing table
+use college;
+
+
+-- while updating anythimg in MYSQL there is a safe mode on we need to turn that off. command for that is
+set sql_safe_updates = 0;  -- safe mode off (u can update rows)
+set sql_safe_updates = 1; -- safe mode on (u cannot update now) 
+ 
+update student
+set city = "surat"
+where city = "surst";
+
+update student 
+set name = "shyam"
+where courses = "DBMS";
+
+-- you can't update multiple rows with different values in a single UPDATE statement.
+-- You'll need to issue separate UPDATE statements for each row you want to update. 
+
+
