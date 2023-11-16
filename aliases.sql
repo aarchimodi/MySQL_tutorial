1d39ba9c331568f5d99b4156c9b9610070d58a9f
aarchi <117932380+aarchimodi@users.noreply.github.com>
2023-11-16 14:11:34 +0530

    select s.* from students as s;

--- aliases.sql
+++ aliases.sql
@@ -1,5 +1,5 @@
 
-
+use college;
 -- aliases are used to give a table, or a column in a table, a temporary name.
 -- here id column will be temporarily named as student_count in the table 
 SELECT count(id) as student_count
@@ -7,3 +7,15 @@ FROM student
 GROUP BY city;
 
 
+select * from students
+inner join courses
+on students.id = courses.id;
+
+-- now here instaed of students and courses u can use aliases
+select *  from students as s
+inner join courses as c
+on s.id = c.id;
+
+
+select s.*,c.* from 
+students as s, courses as c;
